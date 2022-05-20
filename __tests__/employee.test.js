892089c4959c82fbcf3217abdc4d8899873efaaa
hkagei <k_dawg_1022@hotmail.com>
2022-05-19 22:01:37 -0600

    establish more code

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,6 +1,15 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Hiro', '19921022', 'k_dawg_1022@hotmail.com');
+    const employee = new Employee('Hiro', 19921022, 'k_dawg_1022@hotmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
 
+test('gets employee name', () => {
+    const employee = new Employee('Hiro', 19921022, 'k_dawg_1022@hotmail.com');
+    
+    expect(employee.name).toEqual(expect.any(String));  
+})
