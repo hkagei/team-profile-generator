8d94dc7a16bf2fa955eb992dbe4737991cf90070
hkagei <k_dawg_1022@hotmail.com>
2022-05-21 11:09:52 -0600

    further updates to index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const ManagerQuestions = [
     {
         type: 'input',
-        name: 'manager name',
+        name: 'managerName',
         message: 'What is your name?',
     },
     {
         type: 'input',
-        name: 'employee id',
+        name: 'employeeId',
         message: 'What is your employee ID?',
     },
     {
@@ -19,22 +21,92 @@ const ManagerQuestions = [
     },
     {
         type: 'input',
-        name: 'office number',
+        name: 'officeNumber',
         message: 'What is your office number?',
     },
     {
         type: 'list',
-        name: 'add more employees',
+        name: 'addMoreEmployees',
         message: "Would you like to add more to your list?",
         choices: ["Engineer", "Intern", "I'm done"]
     },
 
 ];
 
+const EngineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the engineers name?',
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'What is your employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+    },
+    {
+        type: 'list',
+        name: 'addMoreEmployees',
+        message: "Would you like to add more to your list?",
+        choices: ["Engineer", "Intern", "I'm done"]
+    },
+
+];
+
+const InternQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the interns name?',
+    },
+    {
+        type: 'input',
+        name: 'employeeId',
+        message: 'What is your employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is the interns school?',
+    },
+    {
+        type: 'list',
+        name: 'addMoreEmployees',
+        message: "Would you like to add more to your list?",
+        choices: ["Engineer", "Intern", "I'm done"]
+    },
+
+];
+
+let Engineers = [];
 function init() {
     inquirer.prompt(ManagerQuestions).then((responses)=>{
-        console.log(responses)
-        let manager = new Manager()
+        //console.log(responses)
+        let manager = new Manager(responses.managerName, responses.employeeId, responses.email, responses.officeNumber)
+        console.log(manager)
+        if (responses.addMoreEmployees === "Engineer") {
+            inquirer.prompt(EngineerQuestions).then((responses)=>{
+
+            })
+        
+        }
+      
+
 
     })
 }
