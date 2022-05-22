6256cd26e0ac5553b0892975e05b0c0950f2b912
hkagei <k_dawg_1022@hotmail.com>
2022-05-22 17:06:02 -0600

    finish Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,9 @@
 const Engineer = require('../lib/Engineer');
 
 test ('creates an engineer object', () => {
-    const engineer = new Engineer('Hiro', 19921022, 'k_dawg_1022@hotmail.com');
-    
+    const engineer = new Engineer('Hiro', 19921022, 'k_dawg_1022@hotmail.com', 'hkagei');
 
+    expect(engineer.github).toEqual(expect.any(String));
 })
 
 test ('', () => {
