e5e973bd02d8d310bd81051cda68a3a135f5739c
hkagei <k_dawg_1022@hotmail.com>
2022-06-02 20:19:29 -0600

    rewrite template.js

--- src/template.js
+++ src/template.js
@@ -1,77 +1,92 @@
-const clientTeam = team => {
-
-
-    const clientManager = manager => {
-        return `
-        <div class="card employee-card">
-            <div class="card-header">
-                <h2 class="card-title">${manager.getName()}</h2>
-                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-            </div>
-            
-            <div class="card-body">
-                
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${manager.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
-        `
-        // ${manager.managerId}
-    }
-    const clientEngineer = engineer => {
-        return `
-        <div class="card employee-card">
-            <div class="card-header">
-                <h2 class="card-title">${manager.getName()}</h2>
-                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-            </div>
-            
-            <div class="card-body">
-                
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${manager.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
-        `
-        // ${engineer.engineerId}
-        
-        
-    }
-
-    const clientIntern = intern => {
-        return `
-        <div class="card employee-card">
-            <div class="card-header">
-                <h2 class="card-title">${manager.getName()}</h2>
-                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-            </div>
-            
-            <div class="card-body">
-                
-                <ul class="list-group">
-                    <li class="list-group-item">ID: ${manager.getId()}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-                </ul>
-            </div>
-        </div>
-        
-        `
-        // ${intern.internId}
-    }
-
-
-}
-
-
-module.exports = team => {
+const clientTeam = (team) => {
+  const clientManager = (manager) => {
     return `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${manager.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            </div>
+            
+            <div class="card-body">
+                
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${manager.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                    <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+    // ${manager.managerId}
+  };
+  const clientEngineer = (engineer) => {
+    return `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${engineer.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+            </div>
+            
+            <div class="card-body">
+                
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">GitHub ID: ${engineer.getGithub()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+    // ${engineer.engineerId}
+  };
+
+  const clientIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="card-title">${intern.getName()}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+            </div>
+            
+            <div class="card-body">
+                
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${intern.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                </ul>
+            </div>
+        </div>
+        
+        `;
+    // ${intern.internId}
+  };
+
+  const html = [];
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => clientManager(manager))
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => clientEngineer(engineer))
+  );
+
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => clientIntern(intern))
+  );
+
+  return html.join("");
+};
+
+module.exports = (team) => {
+  return `
 
 <!DOCTYPE html>
 <html lang="en">
@@ -79,7 +94,7 @@ module.exports = team => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -89,5 +104,5 @@ ${clientTeam(team)}
 </body>
 </html>
     
-    `
-}
+    `;
+};
