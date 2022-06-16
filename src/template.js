c8d08e9c2383f36c7df25e6acc4e93a5a44b4c59
hkagei <k_dawg_1022@hotmail.com>
2022-06-16 01:21:07 -0600

    update styling

--- src/template.js
+++ src/template.js
@@ -1,7 +1,8 @@
 const clientTeam = (team) => {
   const clientManager = (manager) => {
     return `
-        <div class="card employee-card">
+        <div class="col-4 mt-4">
+            <div class="card h-100">
             <div class="card-header">
                 <h2 class="card-title">${manager.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -16,12 +17,14 @@ const clientTeam = (team) => {
                 </ul>
             </div>
         </div>
+    </div>
         `;
     // ${manager.managerId}
   };
   const clientEngineer = (engineer) => {
     return `
-        <div class="card employee-card">
+    <div class="col-4 mt-4">
+    <div class="card h-100">
             <div class="card-header">
                 <h2 class="card-title">${engineer.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
@@ -36,13 +39,17 @@ const clientTeam = (team) => {
                 </ul>
             </div>
         </div>
+        </div>
         `;
     // ${engineer.engineerId}
   };
 
   const clientIntern = (intern) => {
     return `
-        <div class="card employee-card">
+        
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card employee-card">
             <div class="card-header">
                 <h2 class="card-title">${intern.getName()}</h2>
                 <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
@@ -57,7 +64,7 @@ const clientTeam = (team) => {
                 </ul>
             </div>
         </div>
-        
+    </div>
         `;
     // ${intern.internId}
   };
@@ -94,14 +101,33 @@ module.exports = (team) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Hiro's Team Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header>
+<nav class="navbar" id="navbar">
+    <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+</nav>
+</header>
+<main>
+<div class="container">
+    <div class="row justify-content-center" id="team-cards">
 
 ${clientTeam(team)}
-    
+
+</div>
+</div>
+</main>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </html>
     
     `;
